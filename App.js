12e0dffe911f505f5332b40694e5d34c0ed7053c
Styles-Theandriod <emmanuelajah188@gmail.com>
2023-12-11 22:22:57 +0100

    hello world

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  React  from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from 'react-navigation-stack';
+
+
+import OnboardingScreen from './screens/OnboardingScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const AppStack = createStackNavigator()
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <AppStack.Navigator
+          headerMode="none"
+        >
+
+          
+
+          <AppStack.Screen name='Onboarding' components={OnboardingScreen}/>
+          <AppStack.Screen name='Login' components={LoginScreen}/>
+          
+        </AppStack.Navigator>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
